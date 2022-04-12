d778e5bd42d53aab3ced528c5ec332e5b1ef03a4
SP <tk34moodle@tk34.mskobr.ru>
2022-04-12 09:30:43 +0300

    Сделана задача 7

--- Console1/Program.cs
+++ Console1/Program.cs
@@ -10,6 +10,13 @@ namespace Console1
     {
         static void Main(string[] args)
         {
-        }
+            //Принять с консоли 3 числа и перемножить их, результат вывести на консоль
+            Console.WriteLine("Введите переменные a, b, c");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a*b*c);
+            Console.ReadKey();
+         }
     }
 }
