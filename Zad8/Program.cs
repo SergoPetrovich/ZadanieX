f1e0a5def3e67de12d91556921e6f7cf37b10844
SP <tk34moodle@tk34.mskobr.ru>
2022-04-12 09:34:27 +0300

    Zad8

---
+++ Zad8/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad8
+{
+    internal class Program
+    {
+        //Принять с консоли 2 числа и найти большее из них
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 2 числа");
+
+        }
+    }
+}
